c081c150569e2da7c1ac183bbdcd40c13f14d8f0
Abigail Ramirez <abigailrg03@gmail.com>
2024-06-18 12:35:39 -0600

    Funciones, Refactorización, APIDOM

--- js/js01-tipos-de-datos/js02.js
+++ js/js01-tipos-de-datos/js02.js
@@ -66,7 +66,7 @@ Funciones similares a las funciones expresadas pero:
  - No requiere la palabra reservada function
  - Si tiene una solo instrucción no requiere las llaves {}
  - Si la instrucción es el mismo retorno, no requiere la palabra return
-
+-Solo un parametro no son necesarios los parentesis (opcional).
 sintaxis:
     const nombreVariable = (parametros) => instrucción;
 
@@ -104,8 +104,118 @@ console.log(`El área de rectángulo es ${ areaRectangle( 6, 4) }`); // 24
 
 // Realizar una Arrow function que calcule el impuesto de un valor
 /** TODO calcular el impuesto de un valor */
-const calcularImpuesto = (precio , iva) => precio * iva + precio;
+const calcularImpuesto = (precio , iva) => precio * iva;
 
 
 // caso de uso:
 console.log( `El impuesto de los calcetines es: ${calcularImpuesto(70.50, .16 ) }`)
+
+
+
+// Ejercicios: Convertir las siguientes funciones a Arrow functions.
+
+/** Función 1: Sumar dos números */
+function sumar(a, b) {
+    return a + b;
+}
+console.log( `La suma de 100 + 5 = ${sumar(100,5)}`); // 105
+
+/** Función 2: Verificar si un número es par 
+function esPar(num) {
+    return num % 2 === 0;
+}*/
+//Solo es un parametro, no son necesarios parentesis 
+const esPar=num=>num % 2 === 0;
+
+console.log( `El número 18 es par? ${esPar(18)}`); // true
+console.log( `El número 23 es par? ${esPar(23)}`); // false
+
+/** Función 3: Verificar si tres números terminan con el mismo dígito 
+function mismosUltimosDigitos(num1, num2, num3) {
+    const ultimoDigito1 = num1 % 10;
+    const ultimoDigito2 = num2 % 10;
+    const ultimoDigito3 = num3 % 10;
+    return ultimoDigito1 === ultimoDigito2 && ultimoDigito1 === ultimoDigito3;
+}*/
+
+const mismosUltimosDigitos = (num1, num2, num3) => {
+    const ultimoDigito1 = num1 % 10; //3
+    const ultimoDigito2 = num2 % 10; // 3 es el residuo
+    const ultimoDigito3 = num3 % 10; // 3
+    return ultimoDigito1 === ultimoDigito2 && ultimoDigito1 === ultimoDigito3;
+};
+
+console.log(`El num. 23, 203 y 1013 terminan en 3? ${mismosUltimosDigitos(23,203,1013)}`);
+
+//Refactorización para que el codigo sea mas facil de leer 
+/* 
+const mismosUltimosDigitos = (num1, num2, num3) =>
+    num1 % 10 === num2 % 10 && num1 % 10 === num3 % 10;*/
+
+
+//============================================
+/*
+ ------------ Funciones de Callback -----------------------
+ Es una función(definida, expresada, arrow, anónima) que se pasa 
+ a otra función como argumento.
+ Se pasa en el argumento como referencia ( sin parentesis).
+ */
+
+ // Hacer con arrow function una función que reciba un texto e imprima en consola
+ 
+const printToConsole = (text) => console.log(text);
+
+
+ // Hacer con arrow function una función que reciba un texto e imprima en alert
+ const printToAlert = (text) => alert(text);
+
+// Realizar una función(arrow function) que reciba un texto
+// y que pueda imprimir en consola, alert y quiza en el futuro
+// se agregue otra salida para imprimir.
+
+//Hacer con arrow una funcion que reciba un texto e imprima en H1 "app-title"
+const printToH1 = (text) => document.getElementById("app-title").innerHTML = text;
+
+
+const printMessageIfElse = (text, optionToPrint) => {
+    if(optionToPrint === "console"){
+        printToConsole(text);
+    } else if (optionToPrint === "alert"){ // else if nos ayuda agregar mas opciones de evaluacion
+        printToAlert(text);
+    } else if( optionToPrint === "h1" ){
+        printToH1(text);
+    
+    }else {
+        console.error("Que transa, te equivocaste de opción");
+    }
+
+};
+//Refactorizacion con codigo switch
+const printMessageSwitch = (text, optionToPrint) => {
+/*
+Switch: es una alternativa a if-else. permite ejecutar diferentes bloques de código basandose en el valor de un expresion
+*/
+    switch(optionToPrint){
+        case "console":
+            printToConsole(text);
+            break;
+        case "alert":
+            printToAlert(text);
+            break;
+        case "h1":
+            printToH1(text);
+            break;
+    default:
+        console.error("Opcion incorrecta")
+
+    }
+
+};
+
+//Uso de la función, colocamos los dos argumentos printMessage = (text("Hola, amigas"), optionToPrint ("console"));
+
+//Modificacion en la memoria del navegador
+// Esto lo permite la APIDOM, esto es una modificacin dinámica
+printMessage("Hola, amigas","h1");
+printMessage("no, no (se rie en bolillense)","console");
+printMessage("Tres tristes tigres", "CONSOLE");
