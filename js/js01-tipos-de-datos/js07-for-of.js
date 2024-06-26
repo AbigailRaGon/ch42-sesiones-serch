a7f5396586c459f4e00d202e04dd09a961625d20
Abigail Ramirez <abigailrg03@gmail.com>
2024-06-26 16:21:57 -0600

    ciclo for of y async_await

---
+++ js/js01-tipos-de-datos/js07-for-of.js
@@ -0,0 +1,30 @@
+console.log("js07 async_await");
+
+for (let i = 1; i <=10; i++) {
+    console.log(i);
+    
+}
+// realizar lo anterior utilizando el ciclo while (while loop)
+//mostrar del 1 al 10
+
+let index = 1;
+
+while (index <= 10) {
+    console.log(index);
+    index++;
+};
+
+const numbers = [2,3,4,6,10];
+for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    console.log (element);
+    
+}
+
+//for of
+for(let number of numbers){
+    console.log(number)
+}
+
+//numbers.forEach(fncCallback)
+numbers.forEach((number, index, array)=>console.log(number));
