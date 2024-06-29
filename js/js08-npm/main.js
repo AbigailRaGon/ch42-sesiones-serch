eea89cc38632314ed34d06b73dc90986c9c2754a
Abigail Ramirez <abigailrg03@gmail.com>
2024-06-28 19:56:22 -0600

    added: introduction to NPM

---
+++ js/js08-npm/main.js
@@ -0,0 +1,20 @@
+console.log("GF, esto no es una etapa");
+console.log ("la suma de 3+2: "+ (3+2));
+
+/*
+JSON (JavaScript Object Notation) es un formato de texto ligero para el intercambio de datos. Algunas de sus características incluyen:
+
+-Es un formato de texto plano y fácil de leer.
+-Utiliza una estructura de pares clave-valor para representar datos.
+-Utiliza comillas dobles para encerrar las claves y los valores.
+-Utiliza comas para separar los pares clave-valor.
+-Utiliza corchetes para representar arreglos.
+-Utiliza llaves para representar objetos.
+-Utiliza valores booleanos, números, cadenas, arreglos y objetos para representar datos.
+-Es independiente del lenguaje, ya que se puede utilizar en una variedad de lenguajes de programación.
+-Es ampliamente utilizado en aplicaciones web y móviles para transmitir y recibir datos de un servidor.
+-Es un estándar abierto y no tiene restricciones de uso ni de licencia.
+
+Es importante mencionar que JSON es un subconjunto de JavaScript y por lo tanto, es una forma de representar objetos y arreglos en JavaScript, pero no es lo mismo que JavaScript. JSON solo puede representar un subconjunto de los valores que se pueden representar en JavaScript.
+*/
+
